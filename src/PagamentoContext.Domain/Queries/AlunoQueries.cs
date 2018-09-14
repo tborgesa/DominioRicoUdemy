189b6113eeb55f38e5f6c41875ba787c35221b14
Thiago Borges <tborgesa@gmail.com>
2018-09-13 23:07:32 -0300

    Aula 21 - Criando Queries

---
+++ src/PagamentoContext.Domain/Queries/AlunoQueries.cs
@@ -0,0 +1,14 @@
+﻿using PagamentoContext.Domain.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace PagamentoContext.Domain.Queries
+{
+    public static class AlunoQueries
+    {
+        public static Expression<Func<Aluno,bool>> GetAluno(string documento)
+        {
+            return a => a.Documento.Numero == documento;
+        }
+    }
+}
