186e05adf7d85bb897723852f05ce435bf4de6cb
Thiago Borges <tborgesa@gmail.com>
2018-09-13 21:53:41 -0300

    Aula 18 - Criando repositório de aluno - Através do "RepositoryPattern"

---
+++ src/PagamentoContext.Domain/Repositories/IAlunoRepository.cs
@@ -0,0 +1,11 @@
+﻿using PagamentoContext.Domain.Entities;
+
+namespace PagamentoContext.Domain.Repositories
+{
+    public interface IAlunoRepository
+    {
+        bool DocumentoExiste(string documento);
+        bool EmailExiste(string email);
+        void CriarAssinatura(Aluno aluno);
+    }
+}
