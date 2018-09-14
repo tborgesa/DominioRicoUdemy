1ea41e740d029fc1b778d3de8c7c7e7c9a0cdfa3
Thiago Borges <tborgesa@gmail.com>
2018-09-13 21:46:14 -0300

    Aula 17 - Criar testes para commands

---
+++ src/PagamentoContext.Tests/Commands/CreateAssinaturaBoletoCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PagamentoContext.Domain.Commands;
+
+namespace PagamentoContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateAssinaturaBoletoCommandTests
+    {
+        [TestMethod]
+        public void Erro_Com_Nome_Invalido()
+        {
+            var command = new CreateAssinaturaBoletoCommand();
+
+            command.PrimeiroNome = "";
+            command.UltimoNome = "";
+
+            command.Validate();
+
+            Assert.AreEqual(false, command.Valid);
+        }
+    }
+
+    
+}
